787595a5aa7e4b4bef46ae4bb4682edc23e60fc7
lordvins226 <kevineilboudo54@gmail.com>
2020-09-08 14:36:27 +0000

    :hatching_chick: :tada: initial commit

---
+++ src/app/types.ts
@@ -0,0 +1,13 @@
+export interface Poll{
+    id: number;
+    question: string;
+    results: number[];
+    options: string[];
+    thumbnail: string
+
+}
+
+export interface Voter{
+    id: string;
+    voted:number[];
+}
