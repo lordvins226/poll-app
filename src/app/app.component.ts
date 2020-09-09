7d36b2ea92a5e9503ff02a526010dacfed52e7a5
lordvins226 <kevineilboudo54@gmail.com>
2020-09-09 15:35:35 +0000

    :star: add poll-vote component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   showForm = false;
+
+  polls = [{
+    question: "Do you Like iphone or android?",
+    image: 'https://images.pexels.com/photos/593319/pexels-photo-593319.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=94',
+    votes: [0, 2, 5, 8],
+    voted: true,
+    
+  },
+  {
+    question: "Do you Like Blockchain?",
+    image: 'https://images.pexels.com/photos/593319/pexels-photo-593319.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=94',
+    votes: [0, 2, 5],
+    voted: false,
+  },
+]
 }
