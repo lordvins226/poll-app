e73cd27f4652ff178ec6a354bf9a3d5634404206
lordvins226 <kevineilboudo54@gmail.com>
2020-09-11 14:06:18 +0000

    :up: update poll vote component

--- src/app/poll-vote/poll-vote.component.ts
+++ src/app/poll-vote/poll-vote.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-poll-vote',
   templateUrl: './poll-vote.component.html',
   styleUrls: ['./poll-vote.component.scss']
 })
-export class PollVoteComponent implements OnInit {
+export class PollVoteComponent{
+  options: ['Monday','Tuesday','Wednesday'];
+  voteForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.voteForm = this.fb.group({
+      selected: this.fb.control('',[Validators.required]),
+    });
+   }
 
-  ngOnInit(): void {
+   submitForm(data){
+    console.log("formData:" , data);
   }
 
 }
